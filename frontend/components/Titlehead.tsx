e341f01f5e68a17581db5963de3818731a48dd9e
Abheek Tripathy <abheek.tripathy@gmail.com>
2022-12-31 12:40:10 +0530

    ah

---
+++ frontend/components/Titlehead.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Titlehead = () => {
+  return (
+    <div>
+          <Image src="../public/images/gradbox1.png" alt={'ok'}/>
+    </div>
+  )
+}
+
+export default Titlehead
